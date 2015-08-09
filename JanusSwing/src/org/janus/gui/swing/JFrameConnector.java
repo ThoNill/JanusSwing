991e968d85dd280723469444d0d2883be52c4c7e
Thomas Nill <t.nill@t-online.de>
2015-08-09 10:38:48 +0200

    Anpassung an RootGuiComponent

--- JanusSwing/src/org/janus/gui/swing/JFrameConnector.java
+++ JanusSwing/src/org/janus/gui/swing/JFrameConnector.java
@@ -3,14 +3,21 @@ package org.janus.gui.swing;
 import java.awt.Component;
 import java.awt.Container;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Vector;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+
+import org.janus.data.DataContext;
 import org.janus.gui.basis.GuiComponent;
+import org.janus.gui.basis.RootGuiComponent;
 import org.janus.gui.enums.GuiType;
 
-public class JFrameConnector extends SwingBasisConnector {
+public class JFrameConnector extends SwingBasisConnector implements RootGuiComponent{
 	JFrame frame;
+	private List<GuiComponent> components = null;
+	
 
 	public JFrameConnector(JFrame frame) {
 		super(GuiType.FRAME,frame);
@@ -65,6 +72,22 @@ public class JFrameConnector extends SwingBasisConnector {
 	private void setMenuBarConnector(JMenuBarConnector barConnector) {
 		frame.setJMenuBar(barConnector.getBar());
 	}
+
+	public List<GuiComponent> getAllComponents() {
+		return components;
+	}
+
+	public void setAllComponents(List<GuiComponent> components) {
+		this.components = components;
+	}
 	
+	public void setContext(DataContext context) {
+		super.setContext(context);
+		for(GuiComponent c : components) {
+			if (c != this) {
+			((SwingBasisConnector)c).setContext(context);
+			}
+		}
+	}
 	
 }
